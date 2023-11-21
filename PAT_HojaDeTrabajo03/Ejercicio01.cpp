5e435ec4ecf417252915efd89bd4806e25d64da0
4ndrelo <allanda23456@gmail.com>
2023-11-20 18:22:08 -0600

    Ejercicio 01

--- PAT_HojaDeTrabajo03/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio01.cpp
@@ -2,5 +2,18 @@
 
 int Ejercicio01::findMin(int nums[], int n)
 {
-    return 0;
+    int left = 0;
+    int right = n - 1;
+
+    while (left < right) {
+        int mid = (left + right) / 2;
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        }
+        else {
+            right = mid;
+        }
+    }
+
+    return nums[left];
 }
